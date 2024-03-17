9ee07d75617a65967b769dbd2c2501d2a3c9d583
GuiXvier <guilherme.contatogg@gmail.com>
2024-03-17 01:55:28 -0300

    Todo Cenario Montado

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    public float jumpingPower = 7f;
     private bool isFacingRight = true;
 
     private Animator animator;
